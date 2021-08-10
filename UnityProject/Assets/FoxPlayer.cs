22a084ea2f4f8e88e844ec9c27feddcd5dbc8d25
Anastasia Ranjyue <ranjyue@gmail.com>
2021-08-10 16:37:44 +0800

    EX06 C# 方法與 API
    1.參考影片建立一個 C# 給狐狸，並定義與影片相同的欄位 (使用標題與範圍)
        1. 移動速度
        2. 剛體 Rigidbody2D
        3. 圖片渲染器 SpriteRenderer
    2. 定義一個方法，名稱命名為移動
    3. 使用 Input.GetAxisRaw API 偵測玩家左右按鍵：API 連結
        1. 水平按鈕為 Horizontal 包含左右方向鍵與 A D
    4. 使用 Rigidbody2D.AddForce API 對剛體添加推力：API 連結
        1. 請使用二維向量 new Vector2(X 方向數值, Y 方向數值)
    5. 使用 SpriteRenderer.flipX API 讓狐狸可以翻面：API 連結
    6. 練習完請推送到 GitHub
    
    // 想要讓 方向鍵與AD見同時建立隨著腳色變換方向

---
+++ UnityProject/Assets/FoxPlayer.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxPlayer : MonoBehaviour
+{
+    #region 欄位
+
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 10.5f;
+    [Header("重力"), Range(0, 10)]
+    public float gravity = 0.5f;
+    /// <summary>
+    /// 使用 Input.GetAxisRaw API 偵測玩家左右按鍵
+    /// </summary>
+    //public float Horizontal;
+
+    /// <summary>
+    /// 使用 SpriteRenderer.flipX API 讓狐狸可以翻面
+    /// </summary>
+    public bool flipX;
+
+    #endregion
+
+
+    public Rigidbody2D Rig;
+    public SpriteRenderer Sr;
+
+    private float Horizontal;
+    private float hValue;
+
+    #region 事件
+
+    public void Start()
+    {
+        Rig = GetComponent<Rigidbody2D>();
+    }
+
+    public void Update()
+    {
+        /** 第一次嘗試
+        //float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        //transform.Rotate(0, speed, 0);
+        */
+
+        /** 第二次嘗試
+        //float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        //float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        //transform.Rotate(v, h, 0);
+        */
+
+        /** 第三次嘗試
+        // float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        //float y = Input.GetAxis("Vertical") * Time.deltaTime * speed * gravity;
+        //transform.Translate(x, y, 0);
+        */
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float y = Input.GetAxis("Vertical") * Time.deltaTime * speed * gravity;
+
+        transform.Translate(x, y, 0);
+
+        GetPlayerInputHorizontal();
+        TurnDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(hValue);
+
+        Rig.AddForce(new Vector2(100f, 0f) * Time.deltaTime);
+       
+    }
+
+    #endregion
+
+    #region 方法
+
+    private void GetPlayerInputHorizontal()
+    {
+        hValue = Input.GetAxis("Horizontal");
+    }
+    private void Move(float horizontal)
+    {
+        Vector2 posMove = transform.position + new Vector3(horizontal, -gravity, 0) * speed * Time.fixedDeltaTime;
+        Rig.MovePosition(posMove);
+    }
+
+    private void TurnDirection()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+    #endregion
+
+}
